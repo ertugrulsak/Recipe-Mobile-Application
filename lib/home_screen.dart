a349fd1ffc568eb2f0c976aca01478d5a023815f
ertugrulsak <110829506+ertugrulsak@users.noreply.github.com>
2024-06-09 20:44:00 +0300

    Update home_screen.dart

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -3,14 +3,15 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:myapp/detail_screen.dart';
 
-// ignore: must_be_immutable
+// Ana ekran bileşeni, API'den yemek verilerini alır ve listeler
 class HomeScreen extends StatefulWidget {
   static const routeName = '/homeScreen';
 
-  final String? jsonDatas; // Değiştirildi
+  final String? jsonDatas; // Ana ekran için JSON verisi
 
   HomeScreen({Key? key, required this.jsonDatas});
 
+  // API'den alınan yemek verileri ve detay verileri için değişkenler
   Map<String, dynamic>? jsonData;
   String? jsonDetailData;
   int? idMeal = 0;
@@ -20,21 +21,25 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  late final decodedData;
+  late final decodedData; // API'den gelen JSON verisinin çözümlenmiş hali
+
   @override
   void initState() {
     super.initState();
-    // asyncMethod();
-    decodedData = jsonDecode(widget.jsonDatas!); 
+    // Ana bileşen yüklendiğinde, API'den alınan JSON verisini çözümle
+    decodedData = jsonDecode(widget.jsonDatas!);
   }
 
+  // Asenkron olarak yemek verilerini API'den getirir
   void asyncMethod() async {
     var data = await fetchMeal();
+    // Yeni veri geldiğinde, bileşenin yeniden çizilmesi için setState çağırılır
     setState(() {
       widget.jsonData = data;
     });
   }
 
+  // API'den yemek verilerini getiren asenkron fonksiyon
   Future<Map<String, dynamic>?> fetchMeal() async {
     var request = http.Request('GET', Uri.parse('https://www.themealdb.com/api/json/v1/1/search.php?f=a'));
 
@@ -43,20 +48,24 @@ class _HomeScreenState extends State<HomeScreen> {
     if (response.statusCode == 200) {
       return jsonDecode(await response.stream.bytesToString());
     } else {
+      // Hata durumunda konsola bildirim yazdır
       print(response.reasonPhrase);
       return null;
     }
   }
 
+  // Seçilen yemeğin detay verilerini API'den getirir
   fetchDetailMeal() async {
     var request = http.Request('GET', Uri.parse('https://www.themealdb.com/api/json/v1/1/lookup.php?i=${widget.idMeal}'));
 
     http.StreamedResponse response = await request.send();
 
     if (response.statusCode == 200) {
+      // API'den gelen detay verilerini saklar ve konsola yazdırır
       widget.jsonDetailData = await response.stream.bytesToString();
       print(widget.jsonDetailData);
     } else {
+      // Hata durumunda konsola bildirim yazdır
       print(response.reasonPhrase);
     }
   }
@@ -65,39 +74,40 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Meals'),
-        centerTitle: true,
+        title: Text('Meals'), // Sayfa başlığı
+        centerTitle: true, // Başlık ortalanır
       ),
-      body: decodedData != null
+      body: decodedData != null // Eğer veri varsa
           ? ListView.builder(
               itemCount: decodedData['meals'].length,
               itemBuilder: (context, index) {
                 return Padding(
                   padding: EdgeInsets.only(bottom: 8),
                   child: ListTile(
-                    leading: Image.network(decodedData['meals'][index]['strMealThumb']),
+                    leading: Image.network(decodedData['meals'][index]['strMealThumb']), // Yemek görseli
                     title: InkWell(
                       onTap: () async {
                         setState(() {
+                          // Seçilen yemeğin ID'sini alır
                           widget.idMeal = int.tryParse(decodedData['meals'][index]['idMeal']);
                         });
-                        await fetchDetailMeal();
+                        await fetchDetailMeal(); // Yemek detaylarını getir
                         Navigator.pushNamed(
                           context,
                           DetailScreen.routeName,
-                          arguments: widget.jsonDetailData,
+                          arguments: widget.jsonDetailData, // Detay ekranına detay verilerini gönderir
                         ).then((_) {
-                          asyncMethod();
+                          asyncMethod(); // Geri döndüğünde ana bileşeni yeniler
                         });
                       },
-                      child: Text(decodedData['meals'][index]['strMeal']),
+                      child: Text(decodedData['meals'][index]['strMeal']), // Yemek adı
                     ),
                   ),
                 );
               },
             )
           : Center(
-              child: CircularProgressIndicator(),
+              child: CircularProgressIndicator(), // Veri yüklenirken dönme animasyonu
             ),
     );
   }
