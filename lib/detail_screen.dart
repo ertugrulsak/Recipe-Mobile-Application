ba0454b8b4470f7f8e448d13b6411becc5918133
ertugrulsak <110829506+ertugrulsak@users.noreply.github.com>
2024-06-09 20:42:36 +0300

    Update detail_screen.dart

--- lib/detail_screen.dart
+++ lib/detail_screen.dart
@@ -6,6 +6,7 @@ class DetailScreen extends StatefulWidget {
 
   final String jsonDetailData;
 
+  // DetailScreen için constructor
   const DetailScreen({super.key, required this.jsonDetailData});
 
   @override
@@ -13,11 +14,13 @@ class DetailScreen extends StatefulWidget {
 }
 
 class _DetailScreenState extends State<DetailScreen> {
+  // API'den gelen JSON verisini saklayacak değişken
   late final decodedData;
 
   @override
   void initState() {
     super.initState();
+    // initState içinde gelen JSON verisini çözümleme
     decodedData = jsonDecode(widget.jsonDetailData);
   }
 
@@ -27,6 +30,7 @@ class _DetailScreenState extends State<DetailScreen> {
       appBar: AppBar(
         centerTitle: true,
         title: Text(
+          // Yemeğin adını başlık olarak göster
           decodedData['meals'][0]['strMeal'],
           style: TextStyle(fontWeight: FontWeight.bold),
         ),
@@ -35,6 +39,7 @@ class _DetailScreenState extends State<DetailScreen> {
         child: Column(
           children: [
             Container(
+              // Yemeğin görselini gösteren konteyner
               height: MediaQuery.of(context).size.height * .4,
               width: MediaQuery.of(context).size.width,
               color: Colors.black,
@@ -43,15 +48,17 @@ class _DetailScreenState extends State<DetailScreen> {
                 fit: BoxFit.cover,
               ),
             ),
-            // Display the meal name
+            // Yemek adını ve talimatlarını gösteren bileşenler
             Padding(
               padding: const EdgeInsets.all(8.0),
               child: Column(
                 children: [
+                  // Başlık: "Meal Instructions"
                   Text(
                     'Meal Instructions',
                     style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
                   ),
+                  // Yemek talimatlarını göster
                   Text(
                     decodedData['meals'][0]['strInstructions'],
                     style: TextStyle(fontSize: 16),
